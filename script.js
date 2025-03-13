be5613006bb4b57c5d773f6ce5c9955f6ce4b518
Michael Beaver <mbeaver@users.noreply.github.com>
2025-03-13 19:30:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+let synthA;
+let synthB;
+// const synthC = new Tone.NoiseSynth().toDestination();
+let synthC;
+
+let loopA;
+let loopB;
+let loopC;
+
+let minBPM = 50;
+let defaultBPM = 72;
+let maxBPM = 148;
+let initialized = false;
+let isPlaying = false;
+function handlePlay() {
+  if (!initialized) {
+    synthA = new Tone.FMSynth().toDestination();
+    synthB = new Tone.AMSynth().toDestination();
+    synthC = new Tone.FMSynth().toDestination();
+  }
+
+  if (isPlaying) {
+    Tone.getTransport().cancel();
+    if (loopA) {loopA.stop(); loopA.dispose();}
+    if (loopB) {loopB.stop(); loopB.dispose();}
+    if (loopC) {loopC.stop(); loopC.dispose();}
+    document.getElementById("play").innerHTML = "play something else";
+  } else {
+    document.getElementById("play").innerHTML = "my ears are bleeding please stop";
+    Tone.getTransport().bpm.value = defaultBPM;
+    Math.floor(Math.random() * 2) > 0 ? playRandom() : pleasantHum();
+  }
+  isPlaying = !isPlaying;
+}
+
 function playNote() {
   // create a synth
   const synth = new Tone.Synth().toDestination();
   // play a note from that synth
-  // synth.triggerAttackRelease("C4", "8n");
   const now = Tone.now();
   synth.triggerAttackRelease("C4", "8n", now);
   synth.triggerAttackRelease("E4", "4n", now + 0.5);
@@ -10,9 +44,6 @@ function playNote() {
 }
 
 function pleasantHum() {
-  // create two monophonic synths
-  const synthA = new Tone.FMSynth().toDestination();
-  const synthB = new Tone.AMSynth().toDestination();
   //play a note every quarter-note
   const loopA = new Tone.Loop((time) => {
     synthA.triggerAttackRelease("C2", "8n", time);
@@ -28,12 +59,8 @@ function pleasantHum() {
 }
 
 function playRandom() {
-  // create two monophonic synths
-  const synthA = new Tone.FMSynth().toDestination();
-  const synthB = new Tone.AMSynth().toDestination();
-  // const synthC = new Tone.NoiseSynth().toDestination();
-  const synthC = new Tone.FMSynth().toDestination();
-  
+  Tone.getTransport().bpm.value = Math.floor(Math.random() * (maxBPM - minBPM + 1)) + minBPM;
+
   // play a note every quarter-note
   const loopA = new Tone.Loop((time) => {
     synthA.triggerAttackRelease("C2", "8n", time);
@@ -44,16 +71,29 @@ function playRandom() {
     synthB.triggerAttackRelease("C4", "8n", time);
   }, "4n").start("8n");
 
-  const pentatonic = ["A3", "C3", "A4", "C4", "D4", "E4", "G4"];
-  const sustains = ["1n", "2n", "4n", "8n", "16n"]
+  const majorPentatonic = ["A3", "C3", "A4", "C4", "D4", "E4", "G4"];
+  const minorPentatonic = ["C4", "Eb4", "F4", "G4", "Bb5"];
+  // const sustains = ["1n", "2n", "4n", "8n", "16n"];
+  const longSustains = ["1n", "2n"];
+  const mixedSustains = ["1n", "2n", "4n", "8n", "16n", "32n"];
+  const shortSustains = ["4n", "16n", "32n"];
+
+  var p = Math.floor(Math.random() * 2) > 0 ? majorPentatonic : minorPentatonic;
+  let s;
+  switch (Math.floor(Math.random() * 3)) {
+    case 0: s = longSustains; break;
+    case 1: s = mixedSustains; break;
+    default: s = shortSustains;
+  }
+  
   // play a random pentatonic within the oscillating drone
   const loopC = new Tone.Loop((time) => {
-    var note = pentatonic[Math.floor(Math.random() * pentatonic.length)];
-    var length = sustains[Math.floor(Math.random() * sustains.length)];
+    var note = p[Math.floor(Math.random() * p.length)];
+    var length = s[Math.floor(Math.random() * s.length)];
+    // sustains[Math.floor(Math.random() * sustains.length)];
     synthC.triggerAttackRelease(note, length, time);
   }, "4n").start(0);
 
   // all loops start when the Transport is started
   Tone.getTransport().start();
 }
-
