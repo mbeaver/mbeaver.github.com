90e4f1947a199eb7fd8c3ad94143c2d60bd8718a
Michael Beaver <mbeaver@users.noreply.github.com>
2025-03-13 14:10:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-import { Sampler } from "tone";
+// import { Sampler } from "tone";
+// 
+// const sampler = new Sampler(
+//   {
+//     A1: "A1.mp3"
+//   },
+//   {
+//     onload: () => {
+//       document.querySelector("button").removeAttribute("disabled");
+//     }
+//   }
+// ).toDestination();
 
-const sampler = new Sampler(
-  {
-    A1: "A1.mp3"
-  },
-  {
-    onload: () => {
-      document.querySelector("button").removeAttribute("disabled");
-    }
-  }
-).toDestination();
+// document.querySelector("button").addEventListener("click", () => {
+//   sampler.triggerAttack("A2");
+// });
 
-document.querySelector("button").addEventListener("click", () => {
-  sampler.triggerAttack("A2");
-});
+function playNote() {
+  // create a synth
+  const synth = new Tone.Synth().toDestination();
+  // play a note from that synth
+  synth.triggerAttackRelease("C4", "8n");
+}
